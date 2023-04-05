f716064788e270fd8e2a0b9136ca749d161e3d45
rmls2 <millerrobert182@gmail.com>
2023-04-05 08:04:08 -0300

    add novas funcoes no mod auxiliar

--- modulo_aux.py
+++ modulo_aux.py
@@ -1,5 +1,6 @@
 # modulo criado para gerar uma interface amigavel no terminal para decidir se o cliente vai enviar mensage pura ou arquivo
 
+import time
 data_msg = ''
 arquivo = None
 
@@ -23,29 +24,80 @@ def inicia_cliente():
                 print('resposta incorreta, vamos tentar novamente!\n')
                 continue
 
+# define que tipo de arquivo será recebido pelo servidor, se vai ser mensagem ou aquivo
+
 
 def inicia_servidor():
+
     while True:
-        entrada_servidor = input(
-            'deseja mandar uma data_msg para o servidor? (y/n): ')
+        print('Que tipo de pacote você deseja receber, mensagem ou arquivo?')
+        opcao_de_pacote = input('Digite sua opção: ')
 
-        if entrada_servidor == 'y' or entrada_servidor == 'Y':
-            global data_msg
-            data_msg = 'ok'  # input('digite sua data_msg aqui: ')
-            return True
-        elif entrada_servidor == 'n' or entrada_servidor == 'N':
-            entrada_servidor = input(
-                'então você deseja enviar um arquivo para o servidor? (y/n): ')
-            if entrada_servidor == 'y' or entrada_servidor == 'Y':
-                global arquivo
-                arquivo = input('passe o caminho do arquivo aqui: ')
-                return False
-            else:
-                print('resposta incorreta, vamos tentar novamente!\n')
-                continue
+        if opcao_de_pacote == 'mensagem':
+            return opcao_de_pacote
+
+        elif opcao_de_pacote == 'arquivo':
+            return opcao_de_pacote
+        else:
+            print('opção invalida, vamos tentar novamente!')
 
 
-""" if inicio():
-    print('você digitou a data_msg: ', data_msg)
-else:
-    print('o caminho do seu arquivo é: ', arquivo) """
+# função para envio de arquivo
+def envia_arquivo(socket_udp, arquivo, ip_porta):
+
+    file = open(arquivo, 'rb')
+    # o método read(1024) vai fazer a leitura dos primeiros 1024 bytes do arquivo
+    data = file.read(1024)
+    while data:
+        # envia a mensage codificada em bytes para o servidor destino
+        socket_udp.sendto(data, ip_porta)
+        # gera um pequeno atraso antes da leitura do próximo pacote, para não sobreescrever bytes
+        time.sleep(0.001)
+        # vai fazer a leitura dos próximos 1024 bytes do arquivo, se houver.
+        data = file.read(1024)
+        print('enviando pacote...')
+    # fehca o arquivo aberto na linha
+    file.close()
+    print('arquivo enviado com sucesso!')
+
+# função para recebimento de arquivo
+
+
+def recebe_arquivo(socket_udp, arquivo):
+    global serv_Addr
+    # abre o arquivo que será retransmitido para a escrita dos bytes enviado pelo servidor
+    file_reccvd = open(arquivo, 'wb')
+    # recebe o dados do socket, um par (bytes, endereço), 1024 representa a capacidade do pacote
+    data_rrecvd, serv_Addr = socket_udp.recvfrom(1024)
+    # o bloco try servir para escrever os dados recebido nesse aquivo ate gerar uma exceção de timeout
+    # isso vai servir para que quando o servidor termine de transmitir o arquivo guardado o código saia do loop
+    try:
+        while data_rrecvd:
+            print('escrevendo os dados...')
+            # escreve os dados no arquivo criado
+            file_reccvd.write(data_rrecvd)
+            print('recebendo pacote retransmitido pelo servidor...')
+            # define um tempo limite para bloquear operaçoes de socket, gerando a exceção timeout
+            socket_udp.settimeout(2)
+            data_rrecvd, serv_Addr = socket_udp.recvfrom(1024)
+    except TimeoutError:
+        print('arquivo enviado pelo servidor e recebido pelo cliente com sucesso')
+
+    file_reccvd.close()
+
+# função que vai enviar mensagem pro servidor
+
+
+def envia_mensagem(socket_udp, mensagem, ip_porta):
+    # envia a mensage codificada em bytes para o servidor destino
+    socket_udp.sendto(mensagem.encode(), ip_porta)
+    print('enviando pacote...')
+
+
+def recebe_mensagem(socket_udp):
+    global serv_Addr
+    # recebe o dados do socket, um par (bytes, endereço), 1024 representa o tamanho do pacote recebido
+    mensagem_serv, serv_Addr = socket_udp.recvfrom(1024)
+    # decodifica os bytes recebidos, nesse caso, o método decode() retorna uma string
+    mensagem_serv = mensagem_serv.decode()
+    return mensagem_serv
