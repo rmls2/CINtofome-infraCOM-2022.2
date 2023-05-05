9eecd4bc84866830e5a721bcde35b826e8c022c5
rmls2 <millerrobert182@gmail.com>
2023-05-05 17:02:56 -0300

    .

---
+++ teste3.py
@@ -0,0 +1,35 @@
+# brincando com json
+
+import json
+
+carros_jason = '{"marca": "fiat", "modelo": "palio", "cor": "vermelho"}'
+
+carros = json.loads(carros_jason)  # o objeto agora é da classe dict
+
+
+""" for k, v in carros.items():
+    print(carros_jason) """
+
+
+""" print(type(carros_jason))
+print(type(carros))
+ """
+
+# objeto dict
+carros = {"marca": "fiat", "modelo": "palio",
+          "cor": "vermelho"}  # objeto da classe dict
+
+# o objeto agora é um json, mas a classe é string
+carros_jason = json.dumps(carros, indent=8, separators=(':', ' - '), sort_keys=True) # parametros adicionais para modificar a estrutura interna do json
+
+# print(carros_jason)
+
+
+jogador_1 = '{ "nome": "robert", "altura": "1.87", "habilidades":{"tocar violao": "True", "programar": "True", "gamer": "False"},"estudos":["ensino medio", "graduacao matematica", "graduacao ciencia da computacao"]}'
+
+# jogador_1_json = json.dumps(jogador_1, indent=4, separators=(':', ' = '), sort_keys=True)
+
+jogador_1_dict = json.loads(jogador_1) #transformando o objeto json em um dict
+
+#acessando a chave 'estudos' cujo o valor é uma lista
+print(jogador_1_dict["estudos"])
