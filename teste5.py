52fdef962024037e726ab594789389bf8d1d8b2c
rmls2 <millerrobert182@gmail.com>
2023-05-07 12:48:21 -0300

    .

---
+++ teste5.py
@@ -0,0 +1,14 @@
+with open('teste_cardapio.txt', 'r') as f:
+    print(f.read())
+    
+acoes = ['casa', 'coracao']
+
+print(acoes)
+
+acoes.remove('casa')
+
+print(acoes)
+
+acoes.remove('coracao')
+
+print(acoes)
